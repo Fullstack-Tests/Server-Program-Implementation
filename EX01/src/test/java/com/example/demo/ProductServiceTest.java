3132bd96cd96309c0f0eb49389b5863a59d493e8
1gyeolpark <marine3682@gmail.com>
2026-06-17 17:14:49 +0900

    V 0.7 박한결 - 일괄등록 중간 실패 시 전체 롤백 테스트 구현

--- EX01/src/test/java/com/example/demo/ProductServiceTest.java
+++ EX01/src/test/java/com/example/demo/ProductServiceTest.java
@@ -3,13 +3,16 @@ package com.example.demo;
 import com.example.demo.Domain.Common.Dtos.ProductDTO;
 import com.example.demo.Domain.Common.Repository.ProductRepository;
 import com.example.demo.Domain.Common.Service.ProductService;
+import com.example.demo.Exception.MyBizException;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-import static org.junit.jupiter.api.Assertions.fail;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 @Transactional   // 테스트 후 자동 롤백
@@ -42,8 +45,22 @@ class ProductServiceTest {
     //  - bulk 리스트 2건 중 2번째를 위 이름과 중복으로 구성
     //  - registerBulk 호출 시 MyBizException 발생(assertThrows)
     //  - 전체 count 가 (등록 1건) 그대로인지(=bulk 0건 저장) 확인
+
+    // 일괄등록 중간 실패 시 전체 롤백 테스트
     @Test
     void registerBulk_중간실패_전체롤백() {
-        fail("TODO: registerBulk 롤백 테스트 구현");
+
+        ProductDTO dto = ProductDTO.builder().name("바게트").price(6000).stock(10).category("제빵").build();  // 중복 유발 상품 1건 DTO 생성
+        productService.register(dto);       // DB에 상품 등록 (count = 1)
+
+        List<ProductDTO> dtoList = List.of(         // 등록할 상품 DTO list로 생성
+                ProductDTO.builder().name("롤빵").price(15000).stock(7).category("제과").build(),       // 정상 상품 생성
+                ProductDTO.builder().name("바게트").price(12000).stock(1).category("제빵").build()       // 이름 중복 상품 DTO 작성, 예외 발생
+        );
+
+        assertThrows(MyBizException.class, () -> productService.registerBulk(dtoList));     // 2번째 상품 이름 중복 예외 발생 확인, 트랜잭션 전체 롤백
+
+        long count = productRepository.count();     // DB에 저장된 전체 상품 개수 확인
+        assertEquals(1, count);             // dtoList 전체 롤백되어 현재 첫 dto 등록 1건만 존재(count = 1)
     }
 }
