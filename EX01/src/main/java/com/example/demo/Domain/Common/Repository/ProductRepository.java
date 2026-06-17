fa2709aec5a5407ee8eacbaa2c40ce26b8e79241
pyj-0313 <pyjun0313@gmail.com>
2026-06-17 17:23:29 +0900

    V 0.6 박영준 - 상품명 중복 검사용 쿼리메서드를 선언

--- EX01/src/main/java/com/example/demo/Domain/Common/Repository/ProductRepository.java
+++ EX01/src/main/java/com/example/demo/Domain/Common/Repository/ProductRepository.java
@@ -9,7 +9,7 @@ public interface ProductRepository extends JpaRepository<Product, Long> {
 
     // TODO: 등록 시 상품명 중복 검사용 쿼리메서드를 선언하라.
     //       (힌트: boolean existsByName(String name) — 선언만 하면 구현은 자동 생성)
-
+    boolean existsByName(String name); //등록 시 상품명 중복 검사용 쿼리메서드를 선언
     // TODO: 수정 시 자기 자신(id)을 제외한 상품명 중복 검사용 쿼리메서드를 선언하라.
     //       (힌트: boolean existsByNameAndIdNot(String name, Long id))
 
